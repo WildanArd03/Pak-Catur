820bf778fdb302de222bee93cfa5669b9f98dc81
Erick Christian <erickchristian48@gmail.com>
2021-02-16 17:04:47 +0700

    added open graph seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,18 @@ import { DefaultSeo } from 'next-seo'
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <DefaultSeo titleTemplate='%s | Matematika' title='Matematika' />
+      <DefaultSeo
+        titleTemplate='%s | Matematika'
+        title='Matematika'
+        description='Kuliah? BSI aja! Hotel? Traveloka'
+        openGraph={{
+          type: 'website',
+          url: 'https://pak-catur.netlify.app/',
+          title: 'Aplikasi Matematika',
+          description: 'Kuliah? BSI aja! Hotel? Traveloka',
+          images: [{ url: 'https://pak-catur.netlify.app/splash.jpg' }],
+        }}
+      />
       <Component {...pageProps} />
     </ChakraProvider>
   )
